d7b1e62db14c6105e130a1fc8c08d7df89060e49
Shailendra Kushwaha <learning4succedding@gmail.com>
2020-01-07 06:36:14 +0200

    added changes

--- src/SkiDayCounter.js
+++ src/SkiDayCounter.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 
-class SkiDayCounter extends React.Component {
+class SkiDayCounter extends Component {
   render() {
     return (
       <div>
-        <section>Skidays</section>
+        <section>
+          <div>
+            <p>Total Days: {this.props.total}</p>
+          </div>
+          <div>
+            <p>Powder Days: {this.props.powder}</p>
+          </div>
+          <div>
+            <p>Backcountry Days: {this.props.backcountry}</p>
+          </div>
+          <div>
+            <p>Goal Days: {this.props.goal}</p>
+          </div>
+        </section>
       </div>
     );
   }
