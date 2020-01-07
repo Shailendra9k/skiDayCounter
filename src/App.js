87b9d4832933aad913bca7b12f6fcbc1e412f1e7
Shailendra Kushwaha <learning4succedding@gmail.com>
2020-01-07 06:28:57 +0200

    component and data added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import SkiDayCounter from "./SkiDayCounter";
 
+let skiData = {
+  total: 50,
+  powder: 20,
+  backcountry: 10,
+  goal: 100
+};
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <SkiDayCounter
+          total={skiData.total}
+          powder={skiData.powder}
+          backcountry={skiData.backcountry}
+          goal={skiData.goal}
+        />
       </header>
     </div>
   );
---
+++ src/SkiDayCounter.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class SkiDayCounter extends React.Component {
+  render() {
+    return (
+      <div>
+        <section>Skidays</section>
+      </div>
+    );
+  }
+}
+export default SkiDayCounter;
