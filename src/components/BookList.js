d8e60de50e57be5d21cdf191c09355461facfeed
Shailendra Kushwaha <learning4succedding@gmail.com>
2020-01-08 23:32:18 +0200

    state added

---
+++ src/components/BookList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+let bookList = [
+  { title: "The Sun Also Rises", author: "Ernest Hemingway", pages: 260 },
+  { title: "Hunger", author: "Roxane Gay", pages: 320 },
+  { title: "White Teeth", author: "Zadie Smith", pages: 305 },
+  {
+    title: "The Innovation Secret of Steve Jobs",
+    author: "Carmine Gallo",
+    pages: 480
+  }
+];
+<Library books={bookList} />;
