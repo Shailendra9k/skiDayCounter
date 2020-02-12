cabcd71d4100ed14e564d29ad2d50d6e98a2ed55
Shailendra Kushwaha <learning4succedding@gmail.com>
2020-02-12 12:03:07 +0200

    data fetched from api

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
 import Book from "./Book";
+// import Library from './Library';
 
 class Library extends Component {
   constructor(props) {
     super(props);
     this.state = {
       open: true,
-      freeBookmark: true
+      freeBookmark: false,
+      hiring: true,
+      data: [],
+      loading: false
     };
     // this.toggleOpenClosed = this.toggleOpenClosed.bind(this);
+  }
+  componentDidMount() {
+    this.setState({ loading: true });
+    fetch("https://hplussport.com/api/products/order/price/sort/asc/qty/1")
+      .then(data => data.json())
+      .then(data => this.setState({ data, loading: false }));
   }
   toggleOpenClosed = () => {
     this.setState(prevState => ({
@@ -20,6 +30,22 @@ class Library extends Component {
     const { books } = this.props;
     return (
       <div>
+        {/* {this.state.hiring ? <Hiring />:<NotHiring />} */}
+        {this.state.loading ? (
+          "loading..."
+        ) : (
+          <div>
+            {this.state.data.map(product => {
+              return (
+                <div>
+                  <h3>Library product of the Week!</h3>
+                  <h4>{product.name}</h4>
+                  <img src={product.image} height={100} alt={"Week Product"} />
+                </div>
+              );
+            })}
+          </div>
+        )}
         <h1>The library is {this.state.open ? "open" : "closed"}</h1>
         <button onClick={this.toggleOpenClosed}>Change</button>
         {books.map((book, i) => (
